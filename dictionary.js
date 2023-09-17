9a61c78f15bb38552b88a43b5677e8eee1c74e08
manimansih <99656925+anime1nation@users.noreply.github.com>
2023-09-17 16:29:23 +0530

    deployed phase

--- dictionary.js
+++ dictionary.js
@@ -12,8 +12,15 @@ export default async function dictonary(search){
            path:search,
            method:'GET'
         })
-        const d = `${meaning.map(({word,meanings})=>{return `Word: ${word} \n${meanings.map(x=>{return `\n\nPart of speech: ${x.partOfSpeech} \n${x.definitions.map(x=>{return `\n\nDefinition : ${x.definition} \nExample : ${x?.example || ""}`})} \n\nSynonyms: ${x?.synonyms || ""} \nAntonyms: ${x?.antonyms || ""}\n`})}`})}`
-        return d
+        const result = `${meaning.map(({word,meanings})=>{return `Word: ${word} \n${meanings.map(x=>{return `\n\nPart of speech: ${x.partOfSpeech} \n${x.definitions.map(x=>{return `\n\nDefinition : ${x.definition} \nExample : ${x?.example || ""}`})} \n\nSynonyms: ${x?.synonyms || ""} \nAntonyms: ${x?.antonyms || ""}\n`})}`})}`
+        
+        const maxLength = 2000;
+        if (result.length > maxLength) {
+            const trimmedString = result.slice(0, maxLength);
+            return trimmedString; // Outputs the trimmed string
+        } else {
+            return result ; // Outputs the original string if it's not longer than maxLength
+        }
     } catch (error) {
      return '"Sorry pal, we couldnt find definitions for the word you were looking for."'
     }
